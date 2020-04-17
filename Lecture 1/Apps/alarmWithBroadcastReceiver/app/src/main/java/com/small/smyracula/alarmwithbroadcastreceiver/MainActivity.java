e840efe345198c40c55d0847a1fd73a194e6f327
Osman Korcan Andaç <>
2020-04-17 12:56:18 +0300

    lecture 1

---
+++ Lecture 1/Apps/alarmWithBroadcastReceiver/app/src/main/java/com/small/smyracula/alarmwithbroadcastreceiver/MainActivity.java
@@ -0,0 +1,37 @@
+package com.small.smyracula.alarmwithbroadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.ok);
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText text = (EditText) findViewById(R.id.time);
+        int i = Integer.parseInt(text.getText().toString());
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (i * 1000), pendingIntent);
+        Toast.makeText(this, "Alarm " + i + " saniyeye kuruldu.",
+                Toast.LENGTH_LONG).show();
+    }
+}
